ff81f947ac7d8489baf4d149b0930814cc7e0cb3
SenDoan <mariathanhsen@gmail.com>
2023-09-19 08:28:16 +0700

    clone ui

---
+++ resources/views/livewire/search-input.blade.php
@@ -0,0 +1,6 @@
+<div class="w-1/2 flex justify-end">
+    <div class="w-full flex">
+        <input class="border-secondary-subtle border-t border-b border-l rounded-start px-2 py-1" type="text" placeholder="Search..."/>
+        <button class="border-r border-t border-l rounded-end px-2 py-1 bg-pink-500 text-white border-pink-500" wire:click="search">Search</button>
+    </div>
+</div>
